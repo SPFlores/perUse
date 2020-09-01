350e1fa067bd6ad2d3e729f3a63cf27317f708e0
SPFlores <sflores92886@gmail.com>
2020-08-31 18:44:00 -0700

    ready for testing, Router, landing ipsum

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
 import LogIn from './pages/Login'
+import SignUp from './pages/Signup'
+import Search from './pages/Search'
+import Navbar from './components/Navbar'
 import './App.css'
 
 const App = _ => {
   return (
     <Router>
-      <Route exact path='/' render={_=> (<Landing />)} />
+      <Navbar />
+      <Route exact path='/' render={_ => (<Landing />)} />
       <Route path='/login' render={_ => (<LogIn />)} />
       <Route path='/signup' render={_ => (<SignUp />)} />
       <Route path='/search' render={_ => (<Search />)} />
     </Router>
-    <div>
-      <p>Here is some text to show it's working </p>
-    </div>
   )
 }
 
--- Client/src/pages/Landing/Landing.js
+++ Client/src/pages/Landing/Landing.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const LandingPage = _ => {
   return (
-    <p>This is the landing page</p>
+    <div>
+      <p>This is the landing page.</p>
+      <p>This is some text to show that ti's working.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat vitae in, dolor assumenda, laudantium unde dolore rerum, corrupti repellat sequi provident quibusdam nisi. Explicabo dolor nesciunt voluptas nisi iure nam.</p>
+    </div>
   )
 }
 
