756a0f9b94c2db3893af5ae694584b2a9e00f8b7
SPFlores <sflores92886@gmail.com>
2020-08-31 18:22:35 -0700

    laying out router

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Landing from './pages/landing'
 import './App.css'
 
 const App = _ => {
   return (
-    <div className="App">
+    // <Router>
+    //   <Route exact path='/' render={_=> (<Landing />)} />
+    //   <Route path='/login' render={_ => (<LogIn />)} />
+    //   <Route path='/signup' render={_ => (<SignUp />)} />
+    //   <Route path='/search' render={_ => (<Search />)} />
+    // </Router>
+    <div>
       <p>Here is some text to show it's working </p>
     </div>
   )
