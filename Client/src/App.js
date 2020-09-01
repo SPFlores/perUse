f1f4cfc146d6311fb7219dcb820e8c386e2c29f8
SPFlores <sflores92886@gmail.com>
2020-08-31 18:35:10 -0700

    spelling again

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Landing from './pages/landing'
+import LogIn from './pages/Login'
 import './App.css'
 
 const App = _ => {
   return (
-    // <Router>
-    //   <Route exact path='/' render={_=> (<Landing />)} />
-    //   <Route path='/login' render={_ => (<LogIn />)} />
-    //   <Route path='/signup' render={_ => (<SignUp />)} />
-    //   <Route path='/search' render={_ => (<Search />)} />
-    // </Router>
+    <Router>
+      <Route exact path='/' render={_=> (<Landing />)} />
+      <Route path='/login' render={_ => (<LogIn />)} />
+      <Route path='/signup' render={_ => (<SignUp />)} />
+      <Route path='/search' render={_ => (<Search />)} />
+    </Router>
     <div>
       <p>Here is some text to show it's working </p>
     </div>
