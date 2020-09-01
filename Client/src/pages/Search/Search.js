22a748825adbc21083bf6357078834993a637def
SPFlores <sflores92886@gmail.com>
2020-08-31 19:04:33 -0700

    laying out search page

--- Client/src/pages/Search/Search.js
+++ Client/src/pages/Search/Search.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const SearchPage = _ => {
   return (
-    <p>This is the search page</p>
+    <div>
+      <p>This is the search page</p>
+      <ul>
+        <li>text about the page</li>
+        <li>button to display all jobs</li>
+        <li>dropdown to set location, type, or skills</li>
+        <li>dynamically showing second dropdown for location, type, or skills (search all jobs, find listed attributes, put in new Set, display)</li>
+        <li>search button that verifies input</li>
+      </ul>
+      <p>Display all jobs matching search criteria, one card for each.</p>
+    </div>
   )
 }
 
