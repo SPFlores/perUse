d6d5ac653924fc837238ca5567662bb18480e8b3
SPFlores <sflores92886@gmail.com>
2020-08-31 18:11:27 -0700

    user routes, file organization

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,8 +3,7 @@ const axios = require('axios')
 module.exports = app => {
   app.post('/signup', (req, res) => {
     axios.post('https://divercity-test.herokuapp.com/register', req.body)
-      .then(({ data }) => {
-        //console.log(data)
+      .then(_ => {
         console.log('user added')
         res.sendStatus(200)
       })
@@ -13,8 +12,7 @@ module.exports = app => {
 
   app.post('/login', (req, res) => {
     axios.post('https://divercity-test.herokuapp.com/login', req.body)
-      .then(({ data }) => {
-        //console.log(data)
+      .then(_ => {
         console.log('user signed in')
         res.sendStatus(200)
       })
