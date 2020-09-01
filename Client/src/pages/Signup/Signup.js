22ecc5b76474ec4ca815f90d427c92555d95e4d9
SPFlores <sflores92886@gmail.com>
2020-08-31 19:19:15 -0700

    laying out signup page

--- Client/src/pages/Signup/Signup.js
+++ Client/src/pages/Signup/Signup.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const SignupPage = _ => {
   return (
-    <p>This is the signup page</p>
+    <div>
+      <p>This is the signup page.</p>
+      <p>Text about benefits of signing up.</p>
+      <p>Three boxes and validating submit button:</p>
+      <li>Name</li>
+      <li>email</li>
+      <li>password (validate against second entry?)</li>
+      <li>remember me box, sets local storage</li>
+      <li>submit button, redirects to landing page if successful, sets session storage</li>
+    </div>
   )
 }
 
