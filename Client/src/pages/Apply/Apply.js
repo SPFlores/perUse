ed5175475641c830c7e0aac1b5338be2223c58ae
SPFlores <sflores92886@gmail.com>
2020-09-02 00:15:18 -0700

    setting up apply API call--BROKEN

--- Client/src/pages/Apply/Apply.js
+++ Client/src/pages/Apply/Apply.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react'
+import qs from 'qs'
 import axios from 'axios'
 
 // Apply takes job ID, title from session storage
@@ -28,14 +29,24 @@ const ApplyPage = _ => {
       } else if (coverLetter.current.value === '') {
         console.log('cover that damn letter')
       } else {
-        // grab token, job ID from sessionStorage
-        const token = sessionStorage.getItem('token')
-        const jobID = sessionStorage.getItem('jobID')
-        // axios to API
-        axios.post('/apply')
+        const token = 'Shh secret token'
+        // sessionStorage.getItem('token')
+        const jobID = '2'
+        const applicationInfo = {
+          motivation: motivation.current.value,
+          cover_letter: coverLetter.current.value
+        }
+        const applicationString = qs.stringify(applicationInfo)
+        const headers = {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+          'Authorization': token
+        }
+        // sessionStorage.getItem('jobID')
+        axios.post(`/apply/${jobID}/${token}`, applicationString, headers)
           .then(_ => {
             // display textbox saying applied
             // show button linking back to search
+            console.log('hit apply post successfully')
           })
           .catch(e => console.log(e))
       }
@@ -43,6 +54,8 @@ const ApplyPage = _ => {
       console.log('user is not logged in')
     }
     console.log('you applied!')
+    console.log(motivation.current.value)
+    console.log(coverLetter.current.value)
   }
 
   return (
---
+++ Routes/applyRoutes.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+
+module.exports = app => {
+  app.post('/apply/:id/:token', (req, res) => {
+    axios.post(`https://divercity-test.herokuapp.com/jobs/${req.params.id}/apply`, req.body, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        'Authorization': `${req.params.token}`
+      }
+    })
+      .then(_ => {
+        res.sendStatus(200)
+      })
+      .catch(e => {
+        console.log(e)
+        console.log(e.config)
+      }
+      )
+  })
+}
