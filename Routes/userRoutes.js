109c13b8281d9def34723ccbc9d69ca96a11911e
SPFlores <sflores92886@gmail.com>
2020-09-01 18:31:40 -0700

    cleaning up route for user signup

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,8 +3,7 @@ const axios = require('axios')
 module.exports = app => {
   app.post('/signup', (req, res) => {
     axios.post('https://divercity-test.herokuapp.com/register', req.body)
-      .then(data => {
-        console.log(data)
+      .then(_ => {
         res.sendStatus(200)
       })
       .catch(e => console.log(e))
