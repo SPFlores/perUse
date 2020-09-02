b9902f0e754ab0b57a6df522f2621281fff8ed93
SPFlores <sflores92886@gmail.com>
2020-09-01 18:34:10 -0700

    landing login/signup buttons

--- Client/src/pages/Landing/Landing.js
+++ Client/src/pages/Landing/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LandingPage = _ => {
   return (
@@ -7,6 +8,14 @@ const LandingPage = _ => {
       <h4>Welcome, NAME</h4>
       <p>Displays dummy text below about what the page is, what you can do.</p>
       <p>Offers buttons to sign up or login (Router).</p>
+      <div>
+        <Link to='/login'>
+          <button>Login</button>
+        </Link>
+        <Link to='/signup'>
+          <button>Signup</button>
+        </Link>
+      </div>
     </div>
   )
 }
